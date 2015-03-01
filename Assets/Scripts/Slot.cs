1bdf478019d146492a93f4d3188c04b90ab1e204
moe-ped <m.berning@web.de>
2015-03-01 18:03:13 +0100

    Slot Script Added

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+	#region IBeginDragHandler implementation
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	#endregion
+
+	#region IDropHandler implementation
+
+	public void OnDrop (PointerEventData eventData)
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	#endregion
+}
